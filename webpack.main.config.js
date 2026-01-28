b7121afbef4f6ee0a605b7867cba03aedbbc9237
Venkatesan <akashvenkat47@gmail.com>
2026-01-28 16:04:38 +0530

    build: externalize sqlcipher and ignore node-pre-gyp AWS/S3 dependencies in webpack configuration.

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -14,12 +14,17 @@ module.exports = {
   externals: {
     keytar: 'commonjs keytar',
     sqlite3: 'commonjs sqlite3',
+    '@journeyapps/sqlcipher': 'commonjs @journeyapps/sqlcipher',
     tedious: 'commonjs tedious',
   },
   plugins: [
     // Ignore optional Sequelize dialect dependencies we don't use
     new webpack.IgnorePlugin({
       resourceRegExp: /^(pg|pg-hstore|pg-native|mysql2|mariadb|oracledb|ibm_db|odbc|better-sqlite3|@vscode\/sqlite3)$/,
+    }),
+    // Ignore optional node-pre-gyp AWS/S3 dependencies (used for publishing binaries, not needed at runtime)
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^(mock-aws-s3|aws-sdk|nock)$/,
     }),
   ],
   resolve: {
@@ -35,6 +40,10 @@ module.exports = {
       'odbc': false,
       'better-sqlite3': false,
       '@vscode/sqlite3': false,
+      // Ignore optional node-pre-gyp dependencies
+      'mock-aws-s3': false,
+      'aws-sdk': false,
+      'nock': false,
     },
   },
 };
